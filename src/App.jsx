91f6addfd503350b906c9e603fcc58f85894f6cf
DimarsMars <dimasrangga620@gmail.com>
2024-12-16 22:18:09 +0800

    router fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Routes, Route } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header';
