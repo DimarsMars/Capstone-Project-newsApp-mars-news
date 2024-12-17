eaed950ee62bf9526dea49108ca03fd2784e904b
DimarsMars <dimasrangga620@gmail.com>
2024-12-17 14:14:43 +0800

    test

--- src/pages/Indonesia.jsx
+++ src/pages/Indonesia.jsx
@@ -74,7 +74,7 @@ const Indonesia = () => {
 
             ))}
           </div>
-          <div className="d-flex justify-content-center align-items-center mt-4 mb-5">
+          <div className="d-flex justify-content-center align-items-center " style={{marginBottom: "120px", marginTop: "30px"}}>
             <button
               className="btn btn-dark me-2 btn-pad"
               onClick={() => handlePageChange(page - 1)}
